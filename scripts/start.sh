f5325e133cf8492aeec18afeaf8ea853cd5079b5
Najeeb <sulaimannajeebadesoji@gmail.com>
2026-01-23 00:09:18 +0000

    Adding start script

--- scripts/start.sh
+++ scripts/start.sh
@@ -9,3 +9,14 @@ check_command docker
 check_command docker-compose || check_command docker compose
 
 log_success "All prerequisites satisfied"
+
+# Start Monitoring Stack (LGTM)
+
+log_info "Starting monitoring stack (Prometheus, Loki, Tempo, Grafana)"
+
+docker compose -f monitoring/compose.yml up -d
+
+log_success "Monitoring stack containers started"
+
+
+
