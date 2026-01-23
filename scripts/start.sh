d20b1854f15a4bedff968a8e0be5bc1e2085092d
Najeeb <sulaimannajeebadesoji@gmail.com>
2026-01-23 00:10:28 +0000

    Adding start script

--- scripts/start.sh
+++ scripts/start.sh
@@ -19,4 +19,24 @@ docker compose -f monitoring/compose.yml up -d
 log_success "Monitoring stack containers started"
 
 
+# Start Airflow Stack
 
+log_info "Starting Airflow stack (Builds custom image from Dockerfile)"
+
+docker compose -f airflow/compose.yml up -d --build
+
+log_success "Airflow stack containers started"
+
+
+log_success "All stacks are up and running"
+
+cat <<EOF
+
+${GREEN} All stacks started successfully, verify with 'docker ps' command${NC}
+
+Access points:
+- Airflow UI:  http://localhost:8080
+- Prometheus:  http://localhost:9090
+- Grafana:     http://localhost:3000
+
+EOF
